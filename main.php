df96678d7e89b10ea9e17c1c8052bf6c66b86587
HMartinez20 <32554944+HMartinez20@users.noreply.github.com>
2019-01-29 16:40:00 -0600

    Create main.php

---
+++ main.php
@@ -0,0 +1,12 @@
+<?php 
+include ("header.php");
+?>
+<!DOCTYPE html>
+<html>
+<h1>StMU Marketplace</h1>
+<div id="menuBar">
+  <button href="https://www.w3schools.com/html/">test</button>
+</div>
+<button type="button" class="btn btn-info">Test Bootstrap Button</button>
+
+</html>
